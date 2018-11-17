9a8acb709120a06318b56e086364e0811eb1a488
Ludvig Strigeus <strigeus@gmail.com>
2018-11-17 19:12:05 +0100

    Allow debug build in build_linux

--- build_linux.sh
+++ build_linux.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 set -e
+
+RELARGS="-O3 -DNDEBUG"
+DBGARGS="-g -D_DEBUG"
+CURARGS="$RELARGS"
+
 clang++-6.0 -c -march=skylake-avx512 crypto/poly1305/poly1305-x64-linux.s crypto/chacha20/chacha20-x64-linux.s
-clang++-6.0 -I . -O3 -DNDEBUG -DWITH_NETWORK_BSD=1 -mssse3 -pthread -lrt -o tunsafe \
+clang++-6.0 -I . $CURARGS -DWITH_NETWORK_BSD=1 -mssse3 -pthread -lrt -o tunsafe \
 tunsafe_amalgam.cpp \
 crypto/aesgcm/aesni_gcm-x64-linux.s \
 crypto/aesgcm/aesni-x64-linux.s \
