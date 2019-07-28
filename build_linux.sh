86839568605dd828178e5894ccc2dbdafa2cdd5b
Peter Bieringer <pb@bieringer.de>
2019-07-28 10:37:32 +0000

    use clang 6 from EL7 SCL if available

--- build_linux.sh
+++ build_linux.sh
@@ -5,8 +5,13 @@ RELARGS="-O3 -DNDEBUG"
 DBGARGS="-g -D_DEBUG"
 CURARGS="$RELARGS"
 
-clang++-6.0 -c -march=skylake-avx512 crypto/poly1305/poly1305-x64-linux.s crypto/chacha20/chacha20-x64-linux.s
-clang++-6.0 -I . $CURARGS -DWITH_NETWORK_BSD=1 -mssse3 -pthread -lrt -o tunsafe \
+CLANGPP="clang++-6.0"
+if [ -x /opt/rh/llvm-toolset-6.0/root/usr/bin/clang++ ]; then
+	CLANGPP="/opt/rh/llvm-toolset-6.0/root/usr/bin/clang++"
+fi
+
+$CLANGPP -c -march=skylake-avx512 crypto/poly1305/poly1305-x64-linux.s crypto/chacha20/chacha20-x64-linux.s
+$CLANGPP -I . $CURARGS -DWITH_NETWORK_BSD=1 -mssse3 -pthread -lrt -o tunsafe \
 tunsafe_amalgam.cpp \
 crypto/aesgcm/aesni_gcm-x64-linux.s \
 crypto/aesgcm/aesni-x64-linux.s \
